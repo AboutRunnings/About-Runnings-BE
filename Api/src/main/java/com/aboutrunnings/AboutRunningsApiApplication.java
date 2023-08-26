c3b882a180e9144c157a610405e3e82c09b8ee3d
Sim_migyeong <80315847+Sim-mi-gyeong@users.noreply.github.com>
2023-08-26 15:50:13 +0900

    style : spotless 설정 추가 및 적용

--- Api/src/main/java/com/aboutrunnings/AboutRunningsApiApplication.java
+++ Api/src/main/java/com/aboutrunnings/AboutRunningsApiApplication.java
@@ -1,9 +1,9 @@
 package com.aboutrunnings;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDateTime;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AboutRunningsApiApplication {
